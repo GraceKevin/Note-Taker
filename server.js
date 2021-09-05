be0e326ecb4a9b9214c4ff52a33d9b7e22f9eadf
Kevin Grace <Kevin@kevins-air.home>
2021-09-05 16:38:12 -0400

    added useAPPs

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 // npm install 
+
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
+const HTMLroutes = require('./routes/api.js');
+const apiRoutes = require('./routes/api.js');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Use APPs
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(HTMLroutes);
+app.use(apiRoutes);
+app.listen(PORT, () =>{
+    console.log(`API server now on ${PORT}!`);
+});
