1e1a7f66a44ebe870eb938bcf7d741cf3cbbd93f
Kevin Grace <Kevin@kevins-air.home>
2021-09-05 18:36:51 -0400

    working fixes

--- routes/api.js
+++ routes/api.js
@@ -10,13 +10,13 @@ const writeFile = util.promisify(fs.writeFile)
 // router - api
 
 router.get("/api/notes", async (req, res) => {
-    const notes = await readFile("db/db.json", "utf-8");
+    const notes = await readFile("db/db.json", "utf8");
     const notesArr = JSON.parse(notes) || [];
     res.json(notesArr);
 })
 
 router.post("/api/notes", async (req, res) => {
-    const notes = await readFile("db/db.json", "utf-8");
+    const notes = await readFile("db/db.json", "utf8");
     const notesArr = JSON.parse(notes) || [];
     console.log(req. body);
     const newNotes = 
