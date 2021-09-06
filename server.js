22c359f914950a0fa955f411d63b25447eb595b4
Kevin Grace <Kevin@kevins-air.home>
2021-09-05 21:04:47 -0400

    update links

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
-// npm install 
-
 const express = require('express');
-
-const HTMLroutes = require('./routes/html');
-const apiRoutes = require('./routes/api.js');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Use APPs
-app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-app.use('/', HTMLroutes);
-app.use('/api', apiRoutes);
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
-app.listen(PORT, () =>{
-    console.log(`API server now on ${PORT}!`);
+// allows the css and js files in the public folder to be served at the same time as the html pages
+app.use(express.static('public'));
+
+// parses incoming string and array data
+app.use(express.urlencoded({ extended: true }));
+
+// parses incoming json data
+app.use(express.json());
+
+// sets the imported routes prefixes to use
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// sets the application to listen in on the available port
+app.listen(PORT, () => {
+    console.log(`API server now running on port ${PORT}.`);
 });
