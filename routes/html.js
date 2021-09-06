a243781806fa37280ec584e402d998e46109536d
Kevin Grace <Kevin@kevins-air.home>
2021-09-05 21:57:45 -0400

    might be working?

---
+++ routes/html.js
@@ -0,0 +1,23 @@
+const router = require('express').Router()
+const { networkInterfaces } = require('os');
+const path = require('path');
+
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+});
+
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"))
+});
+
+router.delete("/api/notes/:id", (req, res) => {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+
+for (let i = 0; i < newNotes.length; i++)
+    if(newNotes[i].id == req.params.id){
+        newNotes.splice(i, 1);
+        break
+    }
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,48 @@
 const express = require('express');
+const path = require('path')
+const fs = require('fs');
+const noteArray = require('./db/db')
+const uniqid = require('uniqid')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
-
-app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.json());
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use(express.static('public'));
 
-// sets the application to listen in on the available port
+// Use Routes
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
+
+app.get('/api/notes', (req, res) => {
+    return res.json(noteArray);
+})
+
+app.post('/api/notes', (req, res) => {
+    let note = req.body;
+    note.id = uniqid();
+    noteArray.push(note);
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(noteArray), null, 2);
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+    const deleteNote = req.params.id;
+    noteArray.forEach((element, index) => {
+        if(element.id === deleteNote){
+            noteArray.splice(index,1);
+        }
+    });
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(noteArray), null, 2);
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
+
 app.listen(PORT, () => {
-    console.log(`API server now running on port ${PORT}!`);
+  console.log(`API server now on port ${PORT}!`);
 });
